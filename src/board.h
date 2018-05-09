4b73fbddb7dff96417008d03cd322db2e2d056ee
is742s08 <is742s08@localhost.localdomain>
2018-05-09 12:58:05 +0700

    Add func in board h

--- src/board.h
+++ src/board.h
@@ -1,6 +1,7 @@
 #ifndef BOARD_H
 #define BOARD_H
-
-void board(char deck[8][8]);
-
+    void reprint_board();
+    int board_func(char *places, int test);
+    void print_board();
+    char **_board();
 #endif
